f9210480d442ec0b86e234a77074c64cfa5e17f6
soumyaaaaaaaaaaaaaaaaaaaaaaa <167617017+soumyaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-05-15 14:54:49 +0530

    stringobjects

---
+++ src/method0/Method0.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package method0;
+
+/**
+ *
+ * @author dell1
+ */
+public class Method0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         /*
+        String str1="Java Program";
+        System.out.println(str1);
+        
+        String str2=new String("JAVA");
+        System.out.println(str2);
+        
+        char c[]={'H','e','l','l','o'};
+        //String str3=new String(c);
+        String str3=new String(c,1,3);
+        System.out.println(str3);
+        
+        byte b[]={65,66,67,68};
+        //String str4=new String(b);
+        String str4=new String(b,1,2);
+        System.out.println(str4);
+        */
+        
+        String str1="Java";
+        //String str2="Java";
+        String str2=new String("Java");
+        System.out.println(str1==str2);
+    }
+    
+}
+
